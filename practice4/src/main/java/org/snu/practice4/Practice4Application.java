5415ae05391f4add85a5a82d3d26ed5a609d8ca6
ryudongil <jules010@naver.com>
2018-11-26 16:20:30 +0900

    init Spring project 4
    practice4

---
+++ practice4/src/main/java/org/snu/practice4/Practice4Application.java
@@ -0,0 +1,12 @@
+package org.snu.practice4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practice4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practice4Application.class, args);
+    }
+}
